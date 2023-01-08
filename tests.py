489b36516011b2d2b1a7ee7f1b1ede9219c3d894
PedroShokk <pribeiroeec@gmail.com>
2023-01-08 13:49:15 +0000

    added tests

--- tests.py
+++ tests.py
@@ -4,3 +4,32 @@ import unittest
 
 class SurfshopTest(unittest.TestCase):
     pass
+
+    def setUp(self):
+      self.cart = surfshop.ShoppingCart()
+
+    def test_add_surfboards_1(self):
+      self.assertEqual(self.cart.add_surfboards(1), f'Successfully added 1 surfboard to cart!' )
+
+
+    def test_add_surfboards_2(self):
+      for i in range(2,5):
+        with unittest.subTest(i=i):
+          self.assertEqual(self.cart.add_surfboards(i), f'Successfully added {i} surfboards to cart!')
+          self.cart = surfshop.ShoppingCart()
+
+    @unittest.skip
+    def test_shoppingCart_Size(self):
+      self.assertRaises(surfshop.TooManyBoardsError, self.cart.add_surfboards, 5)
+
+    @unittest.expectedFailure
+    def test_apply_locals_discount(self):
+      self.assertTrue(self.cart.locals_discount)
+
+
+
+
+
+
+    
+unittest.main()
